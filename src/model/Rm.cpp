a51580aed283cfc2f9c410cb65f7e1409da84004
AbhishekAzadXx77 <reyanshroyabhi77@gmail.com>
2026-08-12 22:42:23 +0530

    rfr-2.4

--- src/model/Rm.cpp
+++ src/model/Rm.cpp
@@ -1,53 +1,84 @@
 #include "Rm.h"
-#include <QtMath>
 
-RadarModel::RadarModel(QObject *parent) : QObject(parent) {}
+#include <cmath>
+
+RadarModel::RadarModel(QObject *parent)
+    : QObject{parent} {}
+
+double RadarModel::sweepAngle() const {
+    return m_sweepAngle;
+}
 
 void RadarModel::setSweepAngle(double angle) {
-    if (std::isnan(angle)) return;
-    angle = fmod(angle, 360.0);
-    if (angle < 0) angle += 360.0;
-    if (!qFuzzyCompare(m_sweepAngle, angle)) {
-        m_sweepAngle = angle;
-        emit sweepAngleChanged();
+    if (std::isnan(angle)) {
+        return;
     }
+    angle = std::fmod(angle, 360.0);
+    if (angle < 0.0) {
+        angle += 360.0;
+    }
+    if (std::abs(m_sweepAngle - angle) < 1e-6) {
+        return;
+    }
+    m_sweepAngle = angle;
+    emit sweepAngleChanged();
 }
 
 QQmlListProperty<Target> RadarModel::targets() {
-    
-    return QQmlListProperty<Target>(this, nullptr, &RadarModel::appendTarget,
-                                    &RadarModel::targetCount, &RadarModel::targetAt, nullptr);
+    return QQmlListProperty<Target>{
+        this,
+        nullptr,
+        &RadarModel::appendTarget,
+        &RadarModel::targetCount,
+        &RadarModel::targetAt,
+        nullptr
+    };
+}
+
+const QList<Target*>& RadarModel::rawTargetList() const {
+    return m_targets;
 }
 
 void RadarModel::addTarget(Target *t) {
     if (t && !m_targets.contains(t)) {
         m_targets.append(t);
         t->setParent(this);
-        emit targetsChanged(); // Notify Repeater of new item
+        emit targetsChanged();
     }
 }
 
 void RadarModel::appendTarget(QQmlListProperty<Target> *list, Target *t) {
-    auto *model = qobject_cast<RadarModel *>(list->object);
-    if (model) model->addTarget(t);
+    if (!list) {
+        return;
+    }
+    auto *model{qobject_cast<RadarModel *>(list->object)};
+    if (model) {
+        model->addTarget(t);
+    }
 }
 
-
 int RadarModel::targetCount(QQmlListProperty<Target> *list) {
-    auto *model = qobject_cast<RadarModel *>(list->object);
+    if (!list) {
+        return 0;
+    }
+    const auto *model{qobject_cast<const RadarModel *>(list->object)};
     return model ? static_cast<int>(model->m_targets.size()) : 0;
 }
 
-
 Target *RadarModel::targetAt(QQmlListProperty<Target> *list, int index) {
-    auto *model = qobject_cast<RadarModel *>(list->object);
-    return model && index >= 0 && index < static_cast<int>(model->m_targets.size())
-           ? model->m_targets.at(index) : nullptr;
+    if (!list) {
+        return nullptr;
+    }
+    auto *model{qobject_cast<RadarModel *>(list->object)};
+    if (model && index >= 0 && index < static_cast<int>(model->m_targets.size())) {
+        return model->m_targets.at(index);
+    }
+    return nullptr;
 }
 
-
 QVariantList RadarModel::targetList() const {
-    QVariantList list;
+    QVariantList list{};
+    list.reserve(m_targets.size());
     for (auto *target : m_targets) {
         list.append(QVariant::fromValue(target));
     }
