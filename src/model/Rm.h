c5ead5342f325e17dd0655664cfe15dd0131f289
AbhishekAzadXx77 <reyanshroyabhi77@gmail.com>
2026-08-12 22:41:50 +0530

    rfr-2.3

--- src/model/Rm.h
+++ src/model/Rm.h
@@ -1,9 +1,10 @@
 #ifndef RADARMODEL_H
 #define RADARMODEL_H
 
+#include <QList>
 #include <QObject>
 #include <QQmlListProperty>
-#include <QList>
+#include <QVariantList>
 #include "Target.h"
 
 class RadarModel : public QObject {
@@ -11,17 +12,17 @@ class RadarModel : public QObject {
     Q_PROPERTY(double sweepAngle READ sweepAngle WRITE setSweepAngle NOTIFY sweepAngleChanged)
     Q_PROPERTY(QQmlListProperty<Target> targets READ targets CONSTANT)
     Q_PROPERTY(QVariantList targetList READ targetList NOTIFY targetsChanged)
+
 public:
     explicit RadarModel(QObject *parent = nullptr);
 
-    double sweepAngle() const { return m_sweepAngle; }
+    double sweepAngle() const;
     void setSweepAngle(double angle);
 
     QQmlListProperty<Target> targets();
-    QVariantList targetList() const;    
+    QVariantList targetList() const;
 
-    // Renamed to avoid conflict with QML-facing QVariantList property
-    const QList<Target*>& rawTargetList() const { return m_targets; }
+    const QList<Target*>& rawTargetList() const;
 
     void addTarget(Target *t);
 
@@ -35,8 +36,8 @@ private:
     static int targetCount(QQmlListProperty<Target> *list);
     static Target *targetAt(QQmlListProperty<Target> *list, int index);
 
-    double m_sweepAngle = 0.0;
-    QList<Target *> m_targets;
+    double m_sweepAngle{0.0};
+    QList<Target*> m_targets{};
 };
 
-#endif // RADARMODEL_H
+#endif
