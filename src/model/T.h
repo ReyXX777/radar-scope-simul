fc4dbb7cba849636ea98c88f1451a4a99069be0b
AbhishekAzadXx77 <reyanshroyabhi77@gmail.com>
2026-08-13 13:28:41 +0530

    rfr-3.1

--- src/model/T.h
+++ src/model/T.h
@@ -1,16 +1,17 @@
 #ifndef T_H
 #define T_H
 
+#include <cstdint>
 #include <QObject>
 
 class Target : public QObject {
     Q_OBJECT
     Q_PROPERTY(double bearing READ bearing WRITE setBearing NOTIFY bearingChanged)
     Q_PROPERTY(double range READ range WRITE setRange NOTIFY rangeChanged)
-    Q_PROPERTY(int id READ id CONSTANT)
+    Q_PROPERTY(std::int32_t id READ id CONSTANT)
 
 public:
-    explicit Target(int id, double bearing, double range, QObject *parent = nullptr);
+    explicit Target(std::int32_t id, double bearing, double range, QObject *parent = nullptr);
 
     double bearing() const;
     void setBearing(double b);
@@ -18,14 +19,14 @@ public:
     double range() const;
     void setRange(double r);
 
-    int id() const;
+    std::int32_t id() const;
 
 signals:
     void bearingChanged();
     void rangeChanged();
 
 private:
-    int m_id{0};
+    std::int32_t m_id{0};
     double m_bearing{0.0};
     double m_range{0.0};
 };
