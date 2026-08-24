bcd72ed6fa15c43f8f325bcfb8e4f3f3d1cb1439
AbhishekAzadXx77 <reyanshroyabhi77@gmail.com>
2026-08-24 23:39:42 +0530

    rfr-7.2

--- src/model/Rm.h
+++ src/model/Rm.h
@@ -6,39 +6,43 @@
 #include <QObject>
 #include <QQmlListProperty>
 #include <QVariantList>
-#include "Target.h"
+#include "T.h"
+
+namespace model {
 
 class RadarModel : public QObject {
     Q_OBJECT
     Q_PROPERTY(double sweepAngle READ sweepAngle WRITE setSweepAngle NOTIFY sweepAngleChanged)
-    Q_PROPERTY(QQmlListProperty<Target> targets READ targets CONSTANT)
+    Q_PROPERTY(QQmlListProperty<::model::Target> targets READ targets CONSTANT)
     Q_PROPERTY(QVariantList targetList READ targetList NOTIFY targetsChanged)
 
 public:
-    explicit RadarModel(QObject *parent = nullptr);
+    explicit RadarModel(::QObject *l_parent = nullptr);
 
     double sweepAngle() const;
-    void setSweepAngle(double angle);
+    void setSweepAngle(double l_angle);
 
     QQmlListProperty<Target> targets();
     QVariantList targetList() const;
 
-    const QList<Target*>& rawTargetList() const;
+    const ::QList<Target*>& rawTargetList() const;
 
-    void addTarget(Target *t);
+    void addTarget(Target *l_t);
 
 signals:
     void sweepAngleChanged();
-    void targetPing(std::int32_t targetId);
+    void targetPing(::std::int32_t targetId);
     void targetsChanged();
 
 private:
-    static void appendTarget(QQmlListProperty<Target> *list, Target *t);
-    static int targetCount(QQmlListProperty<Target> *list);
-    static Target *targetAt(QQmlListProperty<Target> *list, int index);
+    static void appendTarget(QQmlListProperty<Target> *l_list, Target *l_t);
+    static int targetCount(QQmlListProperty<Target> *l_list);
+    static Target *targetAt(QQmlListProperty<Target> *l_list, int l_index);
 
     double m_sweepAngle{0.0e0};
-    QList<Target*> m_targets{};
+    ::QList<Target*> m_targets{};
 };
 
-#endif
+} // namespace model
+
+#endif // RADARMODEL_H
